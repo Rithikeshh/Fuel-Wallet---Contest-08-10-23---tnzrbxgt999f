0aab874c745fb028c72e6b770930c6d097c6a35b
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-10-08 10:26:26 +0530

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch(action.type){
+    
+    case 'add' : {
+      const copyState = [...state, {name :action.name}]
+      return copyState;
+    }
+  }
+};
 
 export default reducer;
